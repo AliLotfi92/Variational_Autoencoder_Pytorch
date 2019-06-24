707ae53f123bfbbdf27ba6b87a66e7ae8d54783a
Ali Lotfi Rezaabad <34145511+AliLotfi92@users.noreply.github.com>
2019-06-24 14:53:55 -0500

    Update and rename Adverserial_VAE.py to Adverserial_VAE_Conv.py

--- Adverserial_VAE_Conv.py
+++ Adverserial_VAE_Conv.py
@@ -1,22 +1,23 @@
-
-
+# In The Name of Allah
 import os
+import math
 import torch
 import random
+import numpy as np
 import torch.nn as nn
 import torch.utils.data
 import torch.optim as optim
 import torch.nn.init as init
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 from torch.utils.data import SubsetRandomSampler
 
 
-
 class Discriminator(nn.Module):
-    def __init__(self, z_dim=10):
+    def __init__(self, z_dim=2):
         super(Discriminator, self).__init__()
         self.z_dim = z_dim
         self.net = nn.Sequential(
@@ -30,7 +31,7 @@ class Discriminator(nn.Module):
             nn.LeakyReLU(0.2, True),
             nn.Linear(1000, 1000),
             nn.LeakyReLU(0.2, True),
-            nn.Linear(1000, 1),
+            nn.Linear(1000, 2),
             nn.Sigmoid()
 
         )
@@ -49,7 +50,7 @@ class Discriminator(nn.Module):
 
 class FactorVAE1(nn.Module):
 
-    def __init__(self, z_dim=10):
+    def __init__(self, z_dim=2):
         super(FactorVAE1, self).__init__()
         self.z_dim = z_dim
         self.encode = nn.Sequential(
@@ -92,14 +93,18 @@ class FactorVAE1(nn.Module):
         eps = std.data.new(std.size()).normal_()
         return eps.mul(std).add_(mu)
 
-    def forward(self, x, no_dec=False):
+    def forward(self, x, no_enc=False, no_dec=False):
 
         stats = self.encode(x)
         mu = stats[:, :self.z_dim]
         logvar = stats[:, self.z_dim:]
         z = self.reparametrize(mu, logvar)
 
-        if no_dec:
+        if no_enc:
+            z = Variable(torch.randn(100, z_dim, 1, 1), requires_grad=False).to(device)
+            return self.decode(z).view(x.size())
+
+        elif no_dec:
             return z.squeeze()
         else:
             x_recon = self.decode(z).view(x.size())
@@ -127,44 +132,55 @@ def kl_divergence(mu, logvar):
     kld = -0.5 * (1 + logvar - mu ** 2 - logvar.exp()).sum(1).mean()
     return kld
 
+def convert_to_display(samples):
+    cnt, height, width = int(math.floor(math.sqrt(samples.shape[0]))), samples.shape[1], samples.shape[2]
+    samples = np.transpose(samples, axes=[1, 0, 2, 3])
+    samples = np.reshape(samples, [height, cnt, cnt, width])
+    samples = np.transpose(samples, axes=[1, 0, 2, 3])
+    samples = np.reshape(samples, [height*cnt, width*cnt])
+    return samples
+
+
 use_cuda = torch.cuda.is_available()
 device = 'cuda' if use_cuda else 'cpu'
 max_iter = int(3000)
-batch_size = 60
-z_dim = 10
+batch_size = 100
+z_dim = 2
 lr_D = 0.001
 beta1_D = 0.9
 beta2_D = 0.999
 gamma = 6.4
 
 training_set = datasets.MNIST('../data', train=True, download=True, transform=transforms.ToTensor())
+test_set = datasets.MNIST('../data', train=False, download=True, transform=transforms.ToTensor())
+
 data_loader = DataLoader(training_set, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_set, batch_size=500, shuffle=True, num_workers=3)
 
 VAE = FactorVAE1().to(device)
 D = Discriminator().to(device)
 optim_VAE = optim.Adam(VAE.parameters(), lr=lr_D, betas=(beta1_D, beta2_D))
 optim_D = optim.Adam(D.parameters(), lr=lr_D, betas=(beta1_D, beta2_D))
 
-
-ones = torch.ones(batch_size, dtype=torch.float, device=device)
-zeros = torch.zeros(batch_size, dtype=torch.float, device=device)
-
+ones = torch.ones(batch_size, dtype=torch.long, device=device)
+zeros = torch.zeros(batch_size, dtype=torch.long, device=device)
 
 for epoch in range(max_iter):
     train_loss = 0
 
-    for batch_idx, (x_true1,_) in enumerate(data_loader):
+    for batch_idx, (x_true, _) in enumerate(data_loader):
 
-        x_true1 = x_true1.to(device)
+        x_true1 = x_true.to(device)
 
-        x_recon, mu, logvar, z = VAE(x_true1)
+        x_recon, mu, logvar, z = VAE(x_true)
 
-        vae_recon_loss = recon_loss(x_recon, x_true1)
+        vae_recon_loss = recon_loss(x_recon, x_true)
         vae_kld = kl_divergence(mu, logvar)
 
         D_z = D(z)
-        vae_tc_loss = (1 - D_z).mean()
-        vae_loss = vae_recon_loss + vae_kld + gamma * vae_tc_loss
+
+        vae_tc_loss = -(D_z[:, :1] - D_z[:, 1:]).mean()
+        vae_loss = vae_recon_loss + gamma * vae_tc_loss
         train_loss += vae_loss.item()
 
         optim_VAE.zero_grad()
@@ -173,29 +189,43 @@ for epoch in range(max_iter):
 
         z_prime = Variable(torch.randn(batch_size, z_dim), requires_grad=False).to(device)
         D_z_pperm = D(z_prime)
-        D_tc_loss = 0.5 * (F.binary_cross_entropy(D_z_pperm, ones) + F.binary_cross_entropy(D_z, zeros))
 
+        D_loss = 0.5 * (F.cross_entropy(D_z_pperm, zeros) + F.cross_entropy(D_z, ones))
         optim_D.zero_grad()
-        D_tc_loss.backward()
+        D_loss.backward()
         optim_D.step()
 
         if batch_idx % 100 == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)] \t  Loss: {:.6f} '.format(epoch, batch_idx * len(x_true1), len(data_loader.dataset),100. * batch_idx / len(data_loader),vae_loss.item() / len(x_true1)))
+            print('Train Epoch: {} [{}/{} ({:.0f}%)] \t  Loss: {:.6f} \t Discriminator Loss: {:.6f} \t Generator Loss: {:.6f}'.format(epoch, batch_idx * len(x_true1),
+                                                                               len(data_loader.dataset),
+                                                                               100. * batch_idx / len(data_loader),
+                                                                               vae_loss.item(),
+                                                                               D_loss.item(),
+                                                                               vae_tc_loss.item()
+                                                                                               ))
+        if batch_idx % 1000 == 0:
+
+            samples = VAE(x_true, no_enc=True)
+            samples = samples.permute(0, 2, 3, 1).contiguous().cpu().data.numpy()
+            plt.imshow(convert_to_display(samples), cmap='Greys_r')
+            plt.show()
 
     print('====> Epoch: {} Average loss: {:.4f}'.format(epoch, train_loss / len(data_loader.dataset)))
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%55
-    
-    
-    
-    
-    
+
+    if z_dim == 2:
+        batch_size_test = 500
+        z_list, label_list = [], []
+
+        for i in range(20):
+            x_test, y_test = iter(test_loader).next()
+            x_test = Variable(x_test, requires_grad=False).to(device)
+            z = VAE(x_test, no_dec=True)
+            z_list.append(z.cpu().data.numpy())
+            label_list.append(y_test.numpy())
+
+        z = np.concatenate(z_list, axis=0)
+        label = np.concatenate(label_list)
+        plt.scatter(z[:, 0], z[:, 1], c=label)
+        plt.show()
+
+    print('====> Epoch: {} Average loss: {:.4f}'.format(epoch, train_loss / len(data_loader.dataset)))
